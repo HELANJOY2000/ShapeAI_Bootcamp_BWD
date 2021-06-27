d0361383f7cfe8e4bdf3023cfe36ced4245ca0ae
HELANJOY2000 <86555819+HELANJOY2000@users.noreply.github.com>
2021-06-27 19:42:01 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript And React.js</h1>
+      <p>A Basic web dev React js Bootcamps </p>
+    </div>
+  );
+}
+
+export default Info;
